2831c8e6ee54c7f6539eb41e791f59ab76067c8b
ReinaMaze <murnamaze456@gmail.com>
2026-03-26 21:29:42 +0100

    Add interactive data visualization components with Recharts

---
+++ admin/app/components/index.ts
@@ -0,0 +1,17 @@
+export { Chart } from './Chart';
+export type { ChartProps } from './Chart';
+
+export { LineChart } from './LineChart';
+export type { LineChartProps, LineChartDataPoint } from './LineChart';
+
+export { BarChart } from './BarChart';
+export type { BarChartProps, BarChartDataPoint } from './BarChart';
+
+export { PieChart } from './PieChart';
+export type { PieChartProps, PieChartDataPoint } from './PieChart';
+
+export { Heatmap } from './Heatmap';
+export type { HeatmapProps, HeatmapDataPoint } from './Heatmap';
+
+export { DashboardGrid } from './DashboardGrid';
+export type { DashboardGridProps } from './DashboardGrid';
