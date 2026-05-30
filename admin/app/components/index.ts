c7c75e2c174bd3e51a7a62d7a344c6627972a16a
vicajohn <qjohn4443@gmail.com>
2026-05-30 04:03:26 +0100

    feat: add EventTypeMultiSelect component for webhook event type selection

--- admin/app/components/index.ts
+++ admin/app/components/index.ts
@@ -6,3 +6,5 @@ export type { SavedSearchListProps, SavedSearch } from './SavedSearchList';
 
 export { SearchHistory } from './SearchHistory';
 export type { SearchHistoryProps, SearchHistoryItem } from './SearchHistory';
+export { EventTypeMultiSelect } from './EventTypeMultiSelect';
+export type { EventType } from './EventTypeMultiSelect';
