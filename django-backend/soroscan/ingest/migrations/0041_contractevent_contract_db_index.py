f1a5d73bb3cbe45497390ce74a22bc5fc6bcdfdd
Ayomide <105206834+AvatarMiiDe@users.noreply.github.com>
2026-06-01 02:44:36 +0100

    Update 0041_contractevent_contract_db_index.py

--- django-backend/soroscan/ingest/migrations/0041_contractevent_contract_db_index.py
+++ django-backend/soroscan/ingest/migrations/0041_contractevent_contract_db_index.py
@@ -8,15 +8,16 @@ class Migration(migrations.Migration):
         ('ingest', '0040_alter_trackedcontract_contract_id'),
     ]
 
-    operations = [
-        migrations.AlterField(
-            model_name='contractevent',
-            name='contract',
-            field=models.ForeignKey(
-                help_text='The contract that emitted this event',
-                on_delete=django.db.models.deletion.CASCADE,
-                related_name='events',
-                to='ingest.trackedcontract',
-            ),
+   operations = [
+    migrations.AlterField(
+        model_name='contractevent',
+        name='contract',
+        field=models.ForeignKey(
+            help_text='The contract that emitted this event',
+            on_delete=django.db.models.deletion.CASCADE,
+            related_name='events',
+            to='ingest.trackedcontract',
+            db_index=True,
         ),
-    ]
+    ),
+]
