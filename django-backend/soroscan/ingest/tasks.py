81671adebc6c6f4f27e82e226ad1481c49a6d073
yinkscss <olayinkashittu2003@gmail.com>
2026-06-24 14:51:51 +0100

    Remove unused CircuitBreakerOpen import

--- django-backend/soroscan/ingest/tasks.py
+++ django-backend/soroscan/ingest/tasks.py
@@ -30,7 +30,7 @@ from django.core.cache import cache
 from django.db.models import Count, F, Max, Min
 from django.utils import timezone
 
-from soroscan.circuit_breaker import CircuitBreakerOpen, execute_with_circuit_breaker
+from soroscan.circuit_breaker import execute_with_circuit_breaker
 
 from .cache_utils import (
     invalidate_event_count_cache,
