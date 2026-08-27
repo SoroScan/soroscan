1e3457363e04cb4525833169c5a9b8114ab89028
MKNas01 <nasirumuhammedkabirux@gmail.com>
2026-08-27 12:46:34 +0100

    chore: remove SDK tutorial example files from frontend source

--- soroscan-frontend/lib/soroscan.ts
+++
@@ -1,6 +0,0 @@
-import { SoroScanClient } from "@soroscan/sdk";
-
-export const soroscan = new SoroScanClient({
-  baseUrl: process.env.NEXT_PUBLIC_SOROSCAN_API_URL || "[https://api.soroscan.io](https://api.soroscan.io)",
-  apiKey: process.env.NEXT_PUBLIC_SOROSCAN_API_KEY,
-});
